a50ff3a5cbe9fbe89c1bb7417c39fb3d5a6410eb
RehamRefaat <78094402+RehamRefaat@users.noreply.github.com>
2023-02-17 13:16:19 +0200

    Add files via upload

---
+++ Type.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell
+{
+    class Type
+    {
+        public void type(string name)
+        {
+            int index = Program.CurrentDirectory.SearchDirectory(name);
+            Program.CurrentDirectory.ReadDirectory();
+            if ( index != -1)
+            {
+
+                int fc =  Program.CurrentDirectory.DirectoryTable[index].firstCluster;
+                int fs = Program.CurrentDirectory.DirectoryTable[index].fileSize;
+                string content = null;
+               // Console.WriteLine(fc);
+                FileEntry file = new FileEntry(name.ToCharArray(),0x0,fc,fs,Program.CurrentDirectory,content);
+                file.ReadFile();
+                Console.WriteLine(file.Content);
+
+            }
+            else
+            {
+                Console.WriteLine("System Can't Find File Specify !");
+            }
+        }
+    }
+}
---
+++ Virtual Disk.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime;
+namespace Shell
+{
+    class Virtual_Disk
+    {   
+      static public Directory Initialize_Virtual_Disk()
+        {
+            Directory root = new Directory("R:".ToCharArray(), 0x10, 5, null);
+            // بتأكد لو الفايل مكتوب فيه فخلاص مش محتاجه اعيد كتابه فيه من تاني 
+           
+            if (!File.Exists("F:\\Shell Randa\\ShellLastRanda\\ShellLast\\ShellLast\\ShellLast\\Shell\\Shell\\Shell\\Virtual_Disk.txt"))
+           // if (stream.Length == 0)
+            {
+               
+                FileStream stream = new FileStream(@"F:\Shell Randa\ShellLastRanda\ShellLast\ShellLast\ShellLast\Shell\Shell\Shell\Virtual_Disk.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                FAT_Table.InitializeFatTable();
+                for (int i = 0; i < 1024 * 1024; i++)
+                {
+                    // بخلي السوبر بلوك باصفار
+                    if (i < 1024)
+                    {
+                        stream.WriteByte((byte)'0');
+                    }
+                    // بحجز مكان للفات تابل
+                    else if (i >= 1024 && i < 1024 * 5)
+                    {
+                        stream.WriteByte((byte)'*');
+                    }
+                    // باقي المساحة بخليها فاضيه
+                    else
+                    {
+                        stream.WriteByte((byte)'#');
+                    }
+                }
+                stream.Close();
+                //FAT_Table.InitializeFatTable();
+                
+                root.WriteDirctory();
+                
+                FAT_Table.WriteFatTable();
+              
+            }
+            else//لو الفايل موجود
+            {
+                root.ReadDirectory();
+                FAT_Table.GetFatTable();
+                
+                //root = new Directory("R:".ToCharArray(), 0x10, 5, null);
+                //root.WriteDirctory();
+               
+               // FAT_Table.PrintFatTable();
+            }
+            return root;
+           
+        }
+        // وظيفة الفانكشن بتستقبل اراي من البايت والاندكس عشان تبدأ تكتب في البلوك اللي مشار له بالاندكس وتكتب فيه الاراي من البايت المبعوت لها
+       static public void WriteBlock(byte[] bytes,int index)
+        {
+           
+            FileStream stream = new FileStream(@"F:\Shell Randa\ShellLastRanda\ShellLast\ShellLast\ShellLast\Shell\Shell\Shell\Virtual_Disk.txt", FileMode.Open, FileAccess.ReadWrite);
+            stream.Seek(index*1024, SeekOrigin.Begin);
+            if (bytes.Length <= 1024)
+            {
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    stream.Write(bytes, i, 1);
+                }
+            }
+            stream.Close();
+        }
+        // وظيفتها تقف عند اندكس معين وتقرا بلوك وتبعتهولي
+       static  public byte[] Get_Block(int index)
+        {
+            byte[] block = new byte[1024];
+            FileStream stream = new FileStream(@"F:\Shell Randa\ShellLastRanda\ShellLast\ShellLast\ShellLast\Shell\Shell\Shell\Virtual_Disk.txt", FileMode.Open, FileAccess.ReadWrite);
+            stream.Seek(index * 1024, SeekOrigin.Begin);
+            for (int i = 0; i < block.Length; i++)
+            {
+                stream.Read(block, i, 1);
+            }
+            stream.Close();
+            return block;
+           
+        }
+    }
+}

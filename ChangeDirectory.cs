8ff9fb6ac1750d9096452877fd6669e66d11ffc0
RehamRefaat <78094402+RehamRefaat@users.noreply.github.com>
2023-02-17 13:11:03 +0200

    Add files via upload

---
+++ ChangeDirectory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell
+{
+    class ChangeDirectory
+    {
+        public void cd(string name)
+        {
+            if (name=="R:\\")
+            {          
+                Program.CurrentPath = "R:";            
+                Program.CurrentDirectory = Virtual_Disk.Initialize_Virtual_Disk();
+            }
+            else
+            {
+                int index = Program.CurrentDirectory.SearchDirectory(name);
+                if (index != -1)
+                {
+                    byte attr = Program.CurrentDirectory.DirectoryTable[index].fileAttribute;
+                    if (attr == 0x10)
+                    {
+                        int fc = Program.CurrentDirectory.DirectoryTable[index].firstCluster;
+                        Directory d = new Directory(name.ToCharArray(), 0x10, fc, Program.CurrentDirectory);
+                        Program.CurrentDirectory = d;
+                        Program.CurrentPath += "\\" + name;
+                        Program.CurrentDirectory.ReadDirectory();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The system cannot find the path specified.");
+                }
+            }
+        }
+
+
+    }
+}
